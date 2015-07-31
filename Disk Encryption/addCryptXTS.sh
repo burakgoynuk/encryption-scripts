9af3c41d9b0f8c779c6216c0b9246ef7537edd87
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2015-07-31 17:16:48 +0300

    XTS scripts added.

---
+++ Disk Encryption/addCryptXTS.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#mkdir ./mountUsb
+# sda1 for board sdb1 for laptop!
+if [ -z ${1} ]
+then
+	echo "The usage of this script is: ./addCrypt /path/to/disk/to/be/encrypted"
+else	
+	encryptedDisk=${1}	
+	cryptsetup --cipher aes-xts-plain64 create newMapper ${encryptedDisk}	
+	#sudo mkfs.ext2 /dev/mapper/newMapper
+	mount -t ext2 /dev/mapper/newMapper /mnt/EncryptedDisk			
+	echo "Encryption Completed!"
+	echo "You can now write to /mnt/EncryptedDisk to write your encrypted disk."
+
+fi
+
+
+
+
+
+
+
+
+
+
+
